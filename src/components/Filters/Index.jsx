da64bf68b9c92c660fc3ac17f69d1c63af6e0236
AngelPs <65132700+AngelPena1@users.noreply.github.com>
2024-03-18 10:40:21 -0400

    removing unnecessary imports

--- src/components/Filters/Index.jsx
+++ src/components/Filters/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Form from "./Form";
 import useToggles from "./hooks/useToggles";
 import useGetDivisions from "./hooks/useGetDivisions";
