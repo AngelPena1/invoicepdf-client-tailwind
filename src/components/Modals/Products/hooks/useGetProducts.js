0e77cd4bd9738ce65aedcd3be8ccfdcae39a6c49
AngelPs <65132700+AngelPena1@users.noreply.github.com>
2024-03-18 10:43:11 -0400

    fixing endpoint

--- src/components/Modals/Products/hooks/useGetProducts.js
+++ src/components/Modals/Products/hooks/useGetProducts.js
@@ -9,7 +9,7 @@ const useGetProducts = ({ limit, searchProduct, inputData }) => {
   const [page, setPage] = useState(0);
   const [search, setSearch] = useState(false);
 
-  const endpoint = `product/company/${companyId}/getall-no_img/page/${page}/limit/${limit}/${inputData?.brand_selected}/${inputData?.category_selected}/${inputData?.subcategory_selected}`;
+  const endpoint = `product/company/${companyId}/getall-no_img/page/${page}/limit/${limit}/${inputData?.brand_selected}/${inputData?.category_selected}/${inputData?.subcategory_selected}/${inputData?.finishes_selected}`;
 
   const endpoint_search = `product/company/${companyId}/get/search/${searchProduct}/page/${page}/limit/${limit}/${inputData?.brand_selected}`;
 
