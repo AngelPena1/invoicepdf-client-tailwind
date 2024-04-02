83eae2ae5a2df8573dad9f84580693e09bbb9456
AngelPs <65132700+AngelPena1@users.noreply.github.com>
2024-04-02 12:35:39 -0400

    creating config provider

---
+++ src/context/ConfigProvider.js
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from "react";
+import { axiosPrivate } from "../api/axios";
+import useAuth from "../hooks/useAuth";
+const ConfigContext = createContext({});
+
+export const ConfigProvider = ({ children }) => {
+  const [config, setConfig] = useState("");
+  const [refetchQuote, setRefetchQuote] = useState(false)
+  const { auth } = useAuth();
+
+  function HandleRefetchQuote() {
+    return setRefetchQuote(!refetchQuote)
+  }
+
+  useEffect(() => {
+    if(auth === "") return
+    axiosPrivate
+      .get(`quote/config/company/${auth?.company?.id}/getall`)
+      .then((res) => {
+        setConfig({
+          ...config,
+          quote: res.data,
+        });
+      });
+  }, [auth, refetchQuote]);
+
+  return (
+    <ConfigContext.Provider value={{ config, setConfig, HandleRefetchQuote }}>
+      {children}
+    </ConfigContext.Provider>
+  );
+};
+
+export default ConfigContext;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./styles/global.scss";
-import './styles/input.css'
+import "./styles/input.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
+import { ConfigProvider } from "./context/ConfigProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <ConfigProvider>
           <Routes>
             <Route path="/*" element={<App />} />
           </Routes>
-        </AuthProvider>
-      </BrowserRouter>
+        </ConfigProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
