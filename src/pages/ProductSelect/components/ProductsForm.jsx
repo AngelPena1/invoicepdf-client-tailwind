433973e77ffca1b2666cad7e63f27c6b1918b844
AngelPs <65132700+AngelPena1@users.noreply.github.com>
2024-04-08 13:08:57 -0400

    Fixing minus quantity

--- src/pages/ProductSelect/components/ProductsForm.jsx
+++ src/pages/ProductSelect/components/ProductsForm.jsx
@@ -175,13 +175,13 @@ const ProductsForm = ({
                       >
                         {product?.description?.substring(0, 42)}
                       </th>
-                      <td className="px-2 py-2 text-center">
+                      <td className="px-2 py-2 text-center select-none">
                         <FontAwesomeIcon
                           className="bg-slate-200 hover:bg-slate-100 p-1 rounded-full relative top-1 right-2 cursor-pointer"
                           icon={faMinus}
                           onClick={() => {
                             HandleQuantityProducts({
-                              productId: product?.id,
+                              local_id: product?.local_id,
                               bool: false,
                             });
                           }}
