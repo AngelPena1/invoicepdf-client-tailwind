ae2d0142809faff6d1afdb18d89d2a6bdd322ec3
AngelPs <65132700+AngelPena1@users.noreply.github.com>
2024-04-09 14:16:05 -0400

    redesigned table

--- src/pages/ProductSelect/components/ProductsForm.jsx
+++ src/pages/ProductSelect/components/ProductsForm.jsx
@@ -9,7 +9,6 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { formatToDecimal } from "../../../utils/formatToDecimal/formatToDecimal";
 import Select from "../../../components/Select/Index";
-import Config from "../../../components/Config";
 
 const ProductsForm = ({
   result,
@@ -125,39 +124,27 @@ const ProductsForm = ({
         <div className="relative overflow-x-auto overflow-y-auto h-72 max-h-72">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500">
             <thead className="text-xs text-gray-700 uppercase bg-slate-300 sticky top-0 z-30">
-              <tr>
-                <th scope="col" className="px-2 py-2">
+              <tr className="">
+                <th scope="col" className="px-2 py-2 w-1/5 text-center">
                   Descripción
                 </th>
-                <th scope="col" className="px-2 py-2 text-center ">
+                <th scope="col" className="px-2 py-2 w-1/5 text-center">
                   Cantidad
                 </th>
                 {toggles?.code && (
-                  <th scope="col" className="px-2 py-2 text-left ">
+                  <th scope="col" className="px-2 py-2 w-1/5 text-center">
                     Código
                   </th>
                 )}
-                <th scope="col" className="px-2 py-2 text-left ">
-                  Marca
-                </th>
-                <th scope="col" className="px-2 py-2 text-left ">
-                  Categoría
-                </th>
-                <th scope="col" className="px-2 py-2 text-left ">
-                  Subcategoría
-                </th>
-                <th scope="col" className="px-2 py-2 text-left ">
-                  Acabado
-                </th>
                 {toggles?.cost && (
-                  <th scope="col" className="px-2 py-2 text-right">
+                  <th scope="col" className="px-2 py-2 w-1/5 text-center">
                     Precio lista
                   </th>
                 )}
-                <th scope="col" className="px-2 py-2 text-right">
+                <th scope="col" className="px-2 py-2 w-1/5 text-center">
                   Precio
                 </th>
-                <th scope="col" className="px-2 py-2 text-center">
+                <th scope="col" className="px-2 py-2 text-center w-1/5">
                   Eliminar
                 </th>
               </tr>
@@ -172,11 +159,11 @@ const ProductsForm = ({
                     >
                       <th
                         scope="row"
-                        className="px-2 py-2 font-medium text-gray-900 whitespace-nowrap"
+                        className="px-2 py-2 font-medium text-gray-900 whitespace-nowrap border-r-2"
                       >
-                        {product?.description?.substring(0, 42)}
+                        {product?.description?.length > 50 ? `${product?.description.substring(0, 50)}...` : `${product?.description}`}
                       </th>
-                      <td className="px-2 py-2 text-center select-none">
+                      <td className="px-2 py-2 select-none text-center border-r-2">
                         <FontAwesomeIcon
                           className="bg-slate-200 hover:bg-slate-100 p-1 rounded-full relative top-1 right-2 cursor-pointer"
                           icon={faMinus}
@@ -200,26 +187,14 @@ const ProductsForm = ({
                         />
                       </td>
                       {toggles?.code && (
-                        <td className="px-2 py-2 text-left">{product?.code}</td>
+                        <td className="px-2 py-2 border-r-2 text-cente">{product?.code}</td>
                       )}
-                      <td className="px-2 py-2 text-left">
-                        {product?.brand?.name?.substring(0, 15)}
-                      </td>
-                      <td className="px-2 py-2 text-left">
-                        {product?.category?.name}
-                      </td>
-                      <td className="px-2 py-2 text-left ">
-                        {product?.subcategory?.name}
-                      </td>
-                      <td className="px-2 py-2 text-left">
-                        {product?.finish?.name}
-                      </td>
                       {toggles?.cost && (
-                        <td className="px-2 py-2 text-right">
+                        <td className="px-2 py-2 text-right border-r-2">
                           {product?.cost}
                         </td>
                       )}
-                      <td className="px-2 py-2 grid place-content-end">
+                      <td className="px-2 py-2 grid justify-end border-r-2">
                         <input
                           name="input-price"
                           className="w-20 text-right p-0 border-0"
@@ -373,7 +348,6 @@ const ProductsForm = ({
           {quoteHasData ? "Guardar cambios" : "Registrar"}
         </button>
       </section>
-      <Config />
     </section>
   );
 };
