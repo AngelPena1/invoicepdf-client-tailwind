fb18baeacb0f9cdf8712b4f897e400f49b937b84
Angelpena1 <Angelpenasantos2017@gmail.com>
2024-04-13 21:22:14 -0400

    removing warning logout

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -58,6 +58,7 @@ const useAxiosPrivate = () => {
       axiosPrivate.interceptors.request.eject(requestInterceptor);
       axiosPrivate.interceptors.response.eject(responseInterceptor);
     };
+    // eslint-disable-next-line
   }, [auth, refresh]);
 
   return axiosPrivate;
