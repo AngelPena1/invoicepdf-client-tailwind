dc84909c641fb4fddc747911011591e4b27eb4b1
Angelpena1 <Angelpenasantos2017@gmail.com>
2024-05-04 18:41:57 -0400

    fixing of showing results

--- src/pages/Products/hooks/useSearchProduct.js
+++ src/pages/Products/hooks/useSearchProduct.js
@@ -3,16 +3,20 @@ import { useState } from "react";
 
 const useSearchProduct = ({ dataArray, searchInput }) => {
   const [result, setResult] = useState([]);
-
+  
   function HandleSearch() {
     const localArray = [];
-    if(!(dataArray.length > 0)) return null
-    dataArray.forEach((data) => {
+    if (!dataArray) return null;
+    if (!(dataArray?.rows?.length > 0)) return null;
+    dataArray?.rows?.forEach((data) => {
       const description = data?.description?.toUpperCase();
       const code = data?.code;
-      if (!searchInput) return;
-      if (description?.includes(searchInput.toUpperCase()))
+      if (!searchInput) {
+        return;
+      }
+      if (description?.includes(searchInput.toUpperCase())) {
         return localArray.push(data);
+      }
       if (code?.includes(searchInput)) return localArray.push(data);
     });
     return setResult(localArray);
