0314d789ea12cd5cd935cfdbe9a02284c700e853
Angelpena1 <angelpenasantos2017@gmail.com>
2024-05-16 16:07:47 -0400

    resolving scroll

--- src/components/Layout/LayoutIndex.jsx
+++ src/components/Layout/LayoutIndex.jsx
@@ -17,15 +17,20 @@ const LayoutIndex = () => {
 
   return (
     <main className="App">
-      {auth !== "" ? <img className="fixed -z-10" src={"https://img.freepik.com/free-vector/blue-curve-background_53876-113113.jpg?w=2000&t=st=1715886845~exp=1715887445~hmac=842ef10d8a5849638ed35f9d50a0b0044a9e5d71b911af9707e78cd595a9e3d7"} alt="" /> : <></>}
-      {auth !== "" ? <Navbar /> : <></>}
-      <div className={auth !== "" ? "relative" : null}>
-        <div className="fixed left-3 right-3 top-20 bottom-4 rounded-lg bg-white p-4">
-          <Outlet />
-          {/* {auth !== "" ? <Config /> : <></>} */}
-        </div>
+    {auth !== "" ? (
+      <div className="fixed inset-0 overflow-hidden">
+        <img className="absolute inset-0 w-full h-full object-cover z-0" src={"https://img.freepik.com/free-vector/blue-curve-background_53876-113113.jpg?w=2000&t=st=1715886845~exp=1715887445~hmac=842ef10d8a5849638ed35f9d50a0b0044a9e5d71b911af9707e78cd595a9e3d7"} alt="" />
       </div>
-    </main>
+    ) : null}
+    {auth !== "" ? <Navbar /> : null}
+  
+    <div className={auth !== "" ? "relative" : ""}>
+      <div className="fixed inset-20 left-3 right-3 bottom-4 overflow-auto rounded-lg bg-white p-4">
+        <Outlet />
+        {/* {auth !== "" ? <Config /> : null} */}
+      </div>
+    </div>
+  </main>
   );
 };
 
