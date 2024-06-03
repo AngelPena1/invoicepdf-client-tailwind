a529195bd10173480474ca47c3292af1a2e6fdae
Angelpena1 <Angelpenasantos2017@gmail.com>
2024-06-02 21:00:35 -0400

    working on using a new server

--- src/pages/Sign/hooks/useHandlePdf.js
+++ src/pages/Sign/hooks/useHandlePdf.js
@@ -1,96 +1,69 @@
 import { useEffect, useState } from "react";
 // import { getDocument } from "pdfjs-dist/build/pdf";
-import jsPDF from "jspdf";
-import SignLogo from "../assets/Firma-Arianny.png";
+// import jsPDF from "jspdf";
+// import SignLogo from "../assets/Firma-Arianny.png";
+import useAuth from "../../../hooks/useAuth";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const useHandlePdf = ({ fileBase64 }) => {
   const [images, setImages] = useState([]);
+  const { auth } = useAuth();
+  const username = auth?.username
 
   function clearImagesArray() {
-    return setImages([])
+    return setImages([]);
+  }
+
+  function descargarPDFDesdeBase64(base64String, nombreArchivo) {
+    // Convertir la cadena base64 en un ArrayBuffer
+    const byteCharacters = atob(base64String);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const fileBlob = new Blob([byteArray], { type: 'application/pdf' });
+  
+    // Crear un enlace de descarga y hacer clic en él
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(fileBlob);
+    link.download = nombreArchivo;
+    link.click();
   }
 
   function HandleSignPdf({ signByPage, coordinates }) {
-    var doc = new jsPDF({
-      format: "letter",
-    });
-
-    images.forEach((image, index) => {
-      doc.addImage(
-        image,
-        "PNG",
-        0,
-        0,
-        doc.internal.pageSize.width,
-        doc.internal.pageSize.height
-      );
-
-      if (signByPage[index]) {
-        const coordinateX = (coordinates[index].x / 178) * 100;
-        const coordinateY = (coordinates[index].y / 176) * 100;
-        doc.addImage(SignLogo, coordinateX, coordinateY, 55, 25);
-      }
-
-      if (index < images.length - 1) {
-        doc.addPage();
-      }
-    });
-
-    doc.save("documento.pdf");
+    axios
+      .post("http://localhost:8000/api/pdf/sign", {
+        coordinates,
+        signByPage,
+        username
+      })
+      .then((res) => {
+        descargarPDFDesdeBase64(res.data?.fileBase64Signed, 'test');
+      })
+      .catch((err) => {
+        if (err.status === 404) return toast.error('Error al firmar el PDF.')
+      });
   }
 
   useEffect(() => {
-    const convertToImages = async () => {
-      import("pdfjs-dist/build/pdf").then(({ getDocument }) => {
-        import("pdfjs-dist/build/pdf.worker.mjs").then(async () => {
-          try {
-            // Extraer la parte base64 de la cadena si tiene el prefijo
-            const base64String = fileBase64.split(",")[1];
+    if (!fileBase64) return;
 
-            // Decodificar la cadena base64
-            const pdfData = Uint8Array.from(atob(base64String), (c) =>
-              c.charCodeAt(0)
-            );
-
-            const loadingTask = getDocument({ data: pdfData });
-            const pdf = await loadingTask.promise;
-
-            const numPages = pdf.numPages;
-            const imageArray = [];
-
-            for (let i = 1; i <= numPages; i++) {
-              const page = await pdf.getPage(i);
-              const viewport = page.getViewport({ scale: 1.5 });
-              const canvas = document.createElement("canvas");
-              const context = canvas.getContext("2d");
-
-              canvas.width = viewport.width;
-              canvas.height = viewport.height;
-
-              const renderContext = {
-                canvasContext: context,
-                viewport: viewport,
-              };
-
-              await page.render(renderContext).promise;
-
-              const imageDataUrl = canvas.toDataURL("image/png");
-              imageArray.push(imageDataUrl);
-            }
-
-            setImages(imageArray);
-          } catch (error) {
-            console.log(error?.message);
-            console.error("Error al convertir el PDF a imágenes:", error);
-          }
-        });
+    axios
+      .post("http://localhost:8000/api/pdf/get-images", {
+        fileBase64,
+        username
+      })
+      .then((res) => {
+        setImages(res.data);
+      })
+      .catch((err) => {
+        if (err.status === 404) return console.log("aqui");
       });
-    };
-
-    if (fileBase64) {
-      convertToImages();
-    }
   }, [fileBase64]);
+
+  // console.log(fileBase64);
 
   return { images, clearImagesArray, HandleSignPdf };
 };
